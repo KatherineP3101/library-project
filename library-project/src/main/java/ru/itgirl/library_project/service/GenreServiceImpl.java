ea1271b457434dca88b9a9d3ffd4dc06f38bf465
KatherineP3101 <kpanova2k@gmail.com>
2025-02-04 22:11:43 +0100

    Applied suggested changes.

--- library-project/src/main/java/ru/itgirl/library_project/service/GenreServiceImpl.java
+++ library-project/src/main/java/ru/itgirl/library_project/service/GenreServiceImpl.java
@@ -5,12 +5,13 @@ import org.springframework.stereotype.Service;
 import ru.itgirl.library_project.dto.AuthorDto;
 import ru.itgirl.library_project.dto.BookDto;
 import ru.itgirl.library_project.dto.GenreDto;
-import ru.itgirl.library_project.model.Book;
+import ru.itgirl.library_project.model.Author;
 import ru.itgirl.library_project.model.Genre;
 import ru.itgirl.library_project.repository.GenreRepository;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -20,20 +21,14 @@ public class GenreServiceImpl implements GenreService {
     @Override
     public GenreDto getGenreById(Long id) {
         Genre genre = genreRepository.findById(id).orElseThrow();
-        return convertToDto(genre);
+        return convertGenreToDto(genre);
     }
 
-    private GenreDto convertToDto(Genre genre) {
+    private GenreDto convertGenreToDto(Genre genre) {
         List<BookDto> bookDtoList = genre.getBooks()
                 .stream()
                 .map(book -> BookDto.builder()
-                        .genre(book.getAuthors().stream()
-                                .map(author -> AuthorDto.builder()
-                                        .id(author.getId())
-                                        .name(author.getName())
-                                        .surname(author.getSurname())
-                                        .build())
-                                .toList().toString())
+                        .authors(convertAuthorListToDto(book.getAuthors()))
                         .name(book.getName())
                         .id(book.getId())
                         .build()
@@ -44,5 +39,27 @@ public class GenreServiceImpl implements GenreService {
                 .name(genre.getName())
                 .books(bookDtoList)
                 .build();
+    }
+
+    private List<AuthorDto> convertAuthorListToDto(Set<Author> authorSet) {
+        List<AuthorDto> authorDtoList = new ArrayList<>();
+        for (Author author : authorSet) {
+            List<BookDto> bookDtoList = author.getBooks()
+                    .stream()
+                    .map(book -> BookDto.builder()
+                            .genre(book.getGenre().getName())
+                            .name(book.getName())
+                            .id(book.getId())
+                            .build()
+                    ).toList();
+            AuthorDto authorDto = AuthorDto.builder()
+                    .books(bookDtoList)
+                    .id(author.getId())
+                    .name(author.getName())
+                    .surname(author.getSurname())
+                    .build();
+            authorDtoList.add(authorDto);
+        }
+        return authorDtoList;
     }
 }

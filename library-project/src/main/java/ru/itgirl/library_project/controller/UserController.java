2ebcb5016ed3ff588abe509386928fb1d377078b
KatherineP3101 <kpanova2k@gmail.com>
2025-02-16 19:23:39 +0100

    Added validation to User and related controller.

--- library-project/src/main/java/ru/itgirl/library_project/controller/UserController.java
+++ library-project/src/main/java/ru/itgirl/library_project/controller/UserController.java
@@ -2,13 +2,18 @@ package ru.itgirl.library_project.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import ru.itgirl.library_project.dto.UserDto;
 import ru.itgirl.library_project.model.Book;
 import ru.itgirl.library_project.model.User;
 import ru.itgirl.library_project.service.UserService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,8 +23,17 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping()
-    public UserDto addNewUser(@RequestBody User user) {
-        return userService.createUser(user);
+    public ResponseEntity<?> addNewUser(@Valid @RequestBody User user, BindingResult result) {
+        if (result.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+            result.getAllErrors().forEach(objectError -> {
+                FieldError fieldError = (FieldError) objectError;
+                errors.put(fieldError.getField(), objectError.getDefaultMessage());
+            });
+
+            return ResponseEntity.badRequest().body(errors);
+        }
+        return ResponseEntity.ok().body(userService.createUser(user));
     }
 
     @GetMapping("/{id}")
